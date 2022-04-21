06e78e7a1c321f9a950292eeba1a7a3de8b0cf0f
Sergio Oseko <osk.sergio@gmail.com>
2022-04-21 10:26:20 -0300

    bug: require command path fix

--- importando_outro_arquivo/principal.rb
+++ importando_outro_arquivo/principal.rb
@@ -1,8 +1,11 @@
-require '../importando_outro_arquivo/classes/pessoa'
-require '../importando_outro_arquivo/classes/carro'
+require "./classes/pessoa"
+require "./classes/carro"
 
 puts "Estou executando o nosso códigos."
 
 
 pessoa1 = Pessoa.new("João")
 carro1  = Carro.new("Fusca", pessoa1)
+
+p pessoa1
+p carro1
